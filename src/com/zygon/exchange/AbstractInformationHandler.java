c076af8608ea6d7dfb38d3db25d73e2ef297a8a4
dcharubini <dcharubini@gmail>
2013-04-11 18:42:37 -0400

    pedantic

--- src/com/zygon/exchange/AbstractInformationHandler.java
+++ src/com/zygon/exchange/AbstractInformationHandler.java
@@ -24,7 +24,7 @@ public abstract class AbstractInformationHandler<T_IN> implements InformationHan
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     @Override
