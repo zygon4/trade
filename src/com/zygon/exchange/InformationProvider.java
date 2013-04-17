5813a10672666ad8e8234b256c1874f4d22010ac
dcharubini <dcharubini@gmail>
2013-04-17 18:42:24 -0400

    harmless cleanup

--- src/com/zygon/exchange/InformationProvider.java
+++ src/com/zygon/exchange/InformationProvider.java
@@ -8,9 +8,7 @@ package com.zygon.exchange;
  *
  * @author zygon
  * 
- * This should not be limited to simply Ticker information, it could be news,
- * crazy-ass Twitter data, etc.
  */
-public interface InformationProvider<FEED_TYPE> {
-    public FEED_TYPE get();
+public interface InformationProvider<T> {
+    public T get();
 }
