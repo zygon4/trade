f25a1c245512938f95a5938d91804fbd9e77c781
dcharubini <dcharubini@gmail>
2013-04-19 17:25:31 -0400

    rename stock indications

--- src/com/zygon/exchange/market/model/indication/technical/NumericIndications.java
+++ src/com/zygon/exchange/market/model/indication/technical/NumericIndications.java
@@ -14,29 +14,29 @@ import com.zygon.exchange.market.model.indication.IndicationListener;
  */
 public class NumericIndications {
     
-    public final IndicationListener<Numeric> AVG_PRICE_5_MIN;
-    public final IndicationListener<Numeric> AVG_PRICE_15_MIN;
-    public final IndicationListener<Numeric> AVG_PRICE_30_MIN;
-    public final IndicationListener<Numeric> AVG_PRICE_60_MIN;
+    public final IndicationListener<Numeric> SMA_5_MIN;
+    public final IndicationListener<Numeric> SMA_15_MIN;
+    public final IndicationListener<Numeric> SMA_30_MIN;
+    public final IndicationListener<Numeric> SMA_60_MIN;
     
     public NumericIndications(String security) {
         
-        this.AVG_PRICE_5_MIN = new NumericIndicationBuilder(security)
+        this.SMA_5_MIN = new NumericIndicationBuilder(security)
                 .set(new Aggregation(Aggregation.Type.AVG, 5, TimeUnits.MINUTES))
                 .set(Classification.PRICE)
                 .build();
         
-        this.AVG_PRICE_15_MIN = new NumericIndicationBuilder(security)
+        this.SMA_15_MIN = new NumericIndicationBuilder(security)
                 .set(new Aggregation(Aggregation.Type.AVG, 15, TimeUnits.MINUTES))
                 .set(Classification.PRICE)
                 .build();
         
-        this.AVG_PRICE_30_MIN = new NumericIndicationBuilder(security)
+        this.SMA_30_MIN = new NumericIndicationBuilder(security)
                 .set(new Aggregation(Aggregation.Type.AVG, 30, TimeUnits.MINUTES))
                 .set(Classification.PRICE)
                 .build();
         
-        this.AVG_PRICE_60_MIN = new NumericIndicationBuilder(security)
+        this.SMA_60_MIN = new NumericIndicationBuilder(security)
                 .set(new Aggregation(Aggregation.Type.AVG, 60, TimeUnits.MINUTES))
                 .set(Classification.PRICE)
                 .build();
