8e1c90a67e87b022b63eaf603d749cf107f52c02
dcharubini <dcharubini@gmail>
2013-04-25 17:26:53 -0400

    Adding some more stock indications

--- src/com/zygon/exchange/market/model/indication/numeric/NumericIndications.java
+++ src/com/zygon/exchange/market/model/indication/numeric/NumericIndications.java
@@ -36,14 +36,25 @@ public class NumericIndications {
         }
     }
     
+    public final NumericIndicationListener SMA_15_SEC;
     public final NumericIndicationListener SMA_5_MIN;
     public final NumericIndicationListener SMA_15_MIN;
     public final NumericIndicationListener SMA_30_MIN;
     public final NumericIndicationListener SMA_60_MIN;
+    public final NumericIndicationListener SMA_240_MIN;
+    
+    public final NumericIndicationListener SMA_1_DAY;
     
     public NumericIndications(String security) {
         
-        Aggregation aggregation = new Aggregation(Aggregation.Type.AVG, 5, TimeUnits.MINUTES);
+        Aggregation aggregation = new Aggregation(Aggregation.Type.AVG, 15, TimeUnits.SECONDS);
+        this.SMA_15_SEC = new NumericIndicationBuilder(security)
+                .set(aggregation)
+                .set(Classification.PRICE)
+                .set(new AggregationStatementProvider(aggregation))
+                .build();
+        
+        aggregation = new Aggregation(Aggregation.Type.AVG, 5, TimeUnits.MINUTES);
         this.SMA_5_MIN = new NumericIndicationBuilder(security)
                 .set(aggregation)
                 .set(Classification.PRICE)
@@ -66,6 +77,20 @@ public class NumericIndications {
         
         aggregation = new Aggregation(Aggregation.Type.AVG, 60, TimeUnits.MINUTES);
         this.SMA_60_MIN = new NumericIndicationBuilder(security)
+                .set(aggregation)
+                .set(Classification.PRICE)
+                .set(new AggregationStatementProvider(aggregation))
+                .build();
+        
+        aggregation = new Aggregation(Aggregation.Type.AVG, 240, TimeUnits.MINUTES);
+        this.SMA_240_MIN = new NumericIndicationBuilder(security)
+                .set(aggregation)
+                .set(Classification.PRICE)
+                .set(new AggregationStatementProvider(aggregation))
+                .build();
+        
+        aggregation = new Aggregation(Aggregation.Type.AVG, 1, TimeUnits.DAYS);
+        this.SMA_1_DAY = new NumericIndicationBuilder(security)
                 .set(aggregation)
                 .set(Classification.PRICE)
                 .set(new AggregationStatementProvider(aggregation))
