e7140933e6f46cf781d0cf3c185333b48b937f9a
dcharubini <dcharubini@gmail>
2013-05-06 18:36:03 -0400

    Rename

--- src/com/zygon/exchange/market/model/indication/numeric/SimpleMovingAverage.java
+++ src/com/zygon/exchange/market/model/indication/numeric/SimpleMovingAverage.java
@@ -11,9 +11,9 @@ import com.zygon.exchange.market.model.indication.Classification;
  *
  * @author zygon
  */
-public class MovingAverage extends NumericIndication {
+public class SimpleMovingAverage extends NumericIndication {
 
-    public MovingAverage(String tradableIdentifier, long timestamp, double value, Aggregation aggregation) {
+    public SimpleMovingAverage(String tradableIdentifier, long timestamp, double value, Aggregation aggregation) {
         super (tradableIdentifier, Classification.PRICE, timestamp, value, aggregation);
         
         if (aggregation.getType() != Aggregation.Type.AVG) {
