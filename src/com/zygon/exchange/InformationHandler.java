97ba0352041f9e23409096d869e21e277f3adf9a
dcharubini <dcharubini@gmail>
2013-05-09 19:53:50 -0400

    Top level "handle" method is now a EventBus subscriber

--- src/com/zygon/exchange/InformationHandler.java
+++ src/com/zygon/exchange/InformationHandler.java
@@ -4,6 +4,8 @@
 
 package com.zygon.exchange;
 
+import com.google.common.eventbus.Subscribe;
+
 /**
  * This is kind of a weak subscriber model.  The intention is to provide
  * a basic building block for pushing data up the stack (raw data -> useful
@@ -12,6 +14,7 @@ package com.zygon.exchange;
  * @author zygon
  */
 public interface InformationHandler<T> {
+    @Subscribe
     public void handle (T t);
     
     // this should stay package scoped and a builder should be used to call this ONLY
