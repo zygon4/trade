05459c2895cd6b9806dfbe59cadd89c49aa661f7
dcharubini <dcharubini@gmail>
2013-05-17 15:21:14 -0400

    comment

--- src/com/zygon/trade/InformationProvider.java
+++ src/com/zygon/trade/InformationProvider.java
@@ -13,6 +13,8 @@ import java.util.Collection;
  */
 public interface InformationProvider<T> {
     public T get();
+    
+    //TBD: move these to the DataProvider?
     public Collection<T> getHistoric();
     public boolean hasHistoricInformation();
 }
