33f1683689099efa631aa5e50ae211ac41a881a6
dcharubini <dcharubini@gmail>
2013-05-17 20:41:29 -0400

    Add isEquals and a couple more Evidence types

--- src/com/zygon/trade/strategy/IndicationProcessor.java
+++ src/com/zygon/trade/strategy/IndicationProcessor.java
@@ -21,7 +21,9 @@ public interface IndicationProcessor<T_IN extends Indication> {
     
     public static enum Evidence {
         BEAR,
-        BULL;
+        BULL,
+        STABLE,
+        VOLATILE;
     }
     
     public static class Response {
@@ -31,6 +33,25 @@ public interface IndicationProcessor<T_IN extends Indication> {
         public Response(Advice advice, Evidence evidence) {
             this.advice = advice;
             this.evidence = evidence;
+        }
+        
+        public boolean isEquals (Response resp) {
+            
+            boolean adviceEqual = false;
+            if (this.getAdvice() != null) {
+                adviceEqual = resp.getAdvice() != null && this.getAdvice() == resp.getAdvice();
+            } else {
+                adviceEqual = resp.getAdvice() == null;
+            }
+            
+            boolean evidenceEqual = false;
+            if (this.getEvidence() != null) {
+                evidenceEqual = resp.getEvidence() != null && this.getEvidence() == resp.getEvidence();
+            } else {
+                evidenceEqual = resp.getEvidence() == null;
+            }
+            
+            return adviceEqual && evidenceEqual;
         }
         
         public Advice getAdvice() {
@@ -51,7 +72,9 @@ public interface IndicationProcessor<T_IN extends Indication> {
 
         @Override
         public String toString() {
-            return String.format("Advice [%s], Evidence [%s]", this.advice.name(), this.evidence.name());
+            return String.format("Advice [%s], Evidence [%s]", 
+                    this.advice != null ? this.advice.name() : "none", 
+                    this.evidence != null ? this.evidence.name() : "none");
         }
     }
     
