715bef5d7ffbefc46ae70cb5d9ac13e5a9a1f6fb
dcharubini <dcharubini@gmail>
2013-05-17 20:44:39 -0400

    Add a nice-to-do

--- src/com/zygon/trade/market/data/DataProvider.java
+++ src/com/zygon/trade/market/data/DataProvider.java
@@ -14,4 +14,14 @@ import java.util.concurrent.TimeUnit;
 public interface DataProvider<T> extends InformationProvider<T> {
     public long getInterval();
     public TimeUnit getUnits();
+    
+    /*
+     * TODO:
+     * /**
+     *  * Returns true if the DataProvider is required to be registered
+     *  * with a scheduled executor.
+     *  *
+     * 
+     * public boolean isPollBased()
+     */
 }
