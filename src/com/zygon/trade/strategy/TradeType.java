3bdd53d30a8cd3226502c462b6f7f31696c9f046
dcharubini <dcharubini@gmail>
2013-05-25 17:02:25 -0400

    Some more meat to the TradeType

--- src/com/zygon/trade/strategy/TradeType.java
+++ src/com/zygon/trade/strategy/TradeType.java
@@ -4,11 +4,29 @@
 
 package com.zygon.trade.strategy;
 
+import com.xeiam.xchange.dto.Order;
+
 /**
  *
  * @author zygon
  */
 public enum TradeType {
-    SHORT,
-    LONG;
+    SHORT (Order.OrderType.ASK, Order.OrderType.BID),
+    LONG (Order.OrderType.BID, Order.OrderType.ASK);
+
+    private Order.OrderType orderType;
+    private Order.OrderType counterOrderType;
+    
+    private TradeType(Order.OrderType orderType, Order.OrderType counterOrderType) {
+        this.orderType = orderType;
+        this.counterOrderType = counterOrderType;
+    }
+
+    public Order.OrderType getCounterOrderType() {
+        return this.counterOrderType;
+    }
+
+    public Order.OrderType getOrderType() {
+        return this.orderType;
+    }
 }
