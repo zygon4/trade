789f066616d6dc44c2136c986f2128f622803250
zygon4 <dcharubini@gmail.com>
2013-10-25 23:15:50 -0400

    optional caching of price

--- src/com/zygon/trade/market/data/interpret/TickerPriceInterpreter.java
+++ src/com/zygon/trade/market/data/interpret/TickerPriceInterpreter.java
@@ -15,31 +15,44 @@ import java.math.RoundingMode;
  */
 public class TickerPriceInterpreter extends TickerInterpreter {
 
+    private final boolean cache;
     private Price previousPrice = null;
 
-    public TickerPriceInterpreter() {
+    public TickerPriceInterpreter(boolean cache) {
         super();
+        this.cache = cache;
+    }
+    
+    public TickerPriceInterpreter() {
+        this (false);
     }
     
     @Override
     public Price[] interpret(Ticker in) {
         
-        if (this.previousPrice == null) {
-            
-            this.previousPrice = new Price (in.getTradableIdentifier(), in.getTimestamp(), 
-                in.getAsk().plus(in.getBid()).dividedBy(2, RoundingMode.UP).getAmount().doubleValue(), Currencies.USD);
-            return new Price[] { this.previousPrice };
-            
+        if (!this.cache) {
+            return new Price[] {
+                new Price (in.getTradableIdentifier(), in.getTimestamp(), 
+                    in.getAsk().plus(in.getBid()).dividedBy(2, RoundingMode.UP).getAmount().doubleValue(), Currencies.USD) 
+            };
         } else {
-            Price newPrice = new Price (in.getTradableIdentifier(), in.getTimestamp(), 
-                in.getAsk().plus(in.getBid()).dividedBy(2, RoundingMode.UP).getAmount().doubleValue(), Currencies.USD);
-            
-            if (newPrice.getValue() != this.previousPrice.getValue()) {
-                this.previousPrice = newPrice;
-                return new Price[] { newPrice };
+            if (this.previousPrice == null) {
+
+                this.previousPrice = new Price (in.getTradableIdentifier(), in.getTimestamp(), 
+                    in.getAsk().plus(in.getBid()).dividedBy(2, RoundingMode.UP).getAmount().doubleValue(), Currencies.USD);
+                return new Price[] { this.previousPrice };
+
+            } else {
+                Price newPrice = new Price (in.getTradableIdentifier(), in.getTimestamp(), 
+                    in.getAsk().plus(in.getBid()).dividedBy(2, RoundingMode.UP).getAmount().doubleValue(), Currencies.USD);
+
+                if (newPrice.getValue() != this.previousPrice.getValue()) {
+                    this.previousPrice = newPrice;
+                    return new Price[] { newPrice };
+                }
             }
+
+            return new Price[]{};
         }
-        
-        return new Price[]{};
     }
 }
