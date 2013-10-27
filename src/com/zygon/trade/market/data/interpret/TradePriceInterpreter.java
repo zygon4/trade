6c743eb7159b40213c8cdc0b946615d0462e43a3
zygon4 <dcharubini@gmail.com>
2013-10-27 16:39:50 -0400

    adding comment for future todo/tbd

--- src/com/zygon/trade/market/data/interpret/TradePriceInterpreter.java
+++ src/com/zygon/trade/market/data/interpret/TradePriceInterpreter.java
@@ -18,6 +18,10 @@ public class TradePriceInterpreter implements Interpreter<Trade> {
     @Override
     public Price[] interpret(Trade in) {
         return new Price[] {
+            
+            // TODO: wrap the xeiam.xchange Trade with a local Trade that
+            // provides a currency
+            
             new Price(in.getTradableIdentifier(), in.getTimestamp().getTime(), 
                 in.getPrice().getAmount().doubleValue(), Currencies.USD)
         };
