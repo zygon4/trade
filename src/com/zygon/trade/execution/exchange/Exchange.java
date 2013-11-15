608bbbecfd0c69ab19f5f4ee6ecc78a29a506f75
zygon4 <dcharubini@gmail.com>
2013-11-15 18:54:08 -0500

    Some incremental changes to the new exchange model

--- src/com/zygon/trade/execution/exchange/Exchange.java
+++ src/com/zygon/trade/execution/exchange/Exchange.java
@@ -9,7 +9,6 @@ import com.xeiam.xchange.dto.marketdata.OrderBook;
 import com.xeiam.xchange.dto.trade.LimitOrder;
 import com.xeiam.xchange.dto.trade.MarketOrder;
 import com.xeiam.xchange.dto.trade.Wallet;
-import com.xeiam.xchange.service.streaming.StreamingExchangeService;
 import com.zygon.trade.execution.AccountController;
 import com.zygon.trade.execution.ExchangeException;
 import com.zygon.trade.execution.OrderBookProvider;
@@ -44,18 +43,13 @@ public abstract class Exchange {
 
     private static final Logger log = LoggerFactory.getLogger(Exchange.class);
 
-    public static class ExchangeEventProcessor extends Thread {
+    private class ExchangeEventProcessor extends Thread {
 
-        private final StreamingExchangeService streamingService;
-        private final ExchangeEventListener listener;
         private volatile boolean running = true;
 
-        public ExchangeEventProcessor(StreamingExchangeService streamingService, ExchangeEventListener listener) {
+        public ExchangeEventProcessor() {
             super();
             super.setDaemon(true);
-
-            this.streamingService = streamingService;
-            this.listener = listener;
         }
 
         @Override
@@ -63,56 +57,16 @@ public abstract class Exchange {
 
             log.debug("ExchangeEventProcessor starting");
 
-            while (this.running) {
+            while (this.running && Exchange.this.isConnected()) {
                 try {
-                    com.xeiam.xchange.service.streaming.ExchangeEvent event = this.streamingService.getNextEvent();
+                    ExchangeEvent event = Exchange.this.getEvent();
 
-                    log.trace("Received event " + event);
-
-                    switch (event.getEventType()) {
-
-                        case CONNECT:
-                            log.info("Connected to exchange");
-                            break;
-
-                        case DISCONNECT:
-                            // TBD: connection task that keeps trying to
-                            // to reconnect
-                            log.info("Disconnected from exchange");
-                            break;
-                            
-                        case ERROR:
-                            log.info("Exchange error: " + event.getData());
-                            break;
-//                    case TICKER:
-//                      Ticker ticker = (Ticker) exchangeEvent.getPayload();
-//                      System.out.println(ticker.toString());
-//                      break;
-
-//                        case USER_ORDER
-//                            break;
-                            
-//                        case TRADE:
-//                            Trade trade = (Trade) event.getPayload();
-//                            // TODO: convert to propert exchange event
-//                            ExchangeEvent exchangeEvent = new ExchangeEvent();
-//                            this.listener.notify(exchangeEvent);
-//                            break;
-//
-//                        case TRADE_LAG:
-//                            MtGoxTradeLag lag = (MtGoxTradeLag) event.getPayload();
-//                            break;
-                            
-//                    case DEPTH:
-//                      OrderBookUpdate orderBookUpdate = (OrderBookUpdate) exchangeEvent.getPayload();
-//                      System.out.println(orderBookUpdate.toString());
-//                      break;
-
-                        default:
-                            // log?
-                            break;
+                    if (event != null) {
+                        log.trace("Received event " + event);
+                        Exchange.this.listener.notify(event);
                     }
-                } catch (InterruptedException ex) {
+                    
+                } catch (ExchangeException ex) {
                     if (this.running) {
                         log.error(null, ex);
                     }
@@ -123,55 +77,73 @@ public abstract class Exchange {
         }
     }
     
-    private final StreamingExchangeService streamingService;
     private ExchangeEventListener listener;
     private ExchangeEventProcessor processor = null;
     private boolean started = false;
 
-    public Exchange(StreamingExchangeService streamingService) {
-        if (streamingService == null) {
-            throw new IllegalArgumentException("No null arguments permitted");
-        }
-        this.streamingService = streamingService;
-    }
+    private final AccountController accntController;
+    private final OrderBookProvider orderBookProvider;
+    private final OrderProvider orderprovider;
+    private final TradeExecutor tradeExecutor;
 
+    public Exchange(
+            AccountController accntController, 
+            OrderBookProvider orderBookProvider, 
+            OrderProvider orderprovider, 
+            TradeExecutor tradeExecutor) {
+        this.accntController = accntController;
+        this.orderBookProvider = orderBookProvider;
+        this.orderprovider = orderprovider;
+        this.tradeExecutor = tradeExecutor;
+    }
+    
     public void cancelOrder(String username, String orderId) throws ExchangeException {
         // TODO: log impl with timestamps
         log.info("{} Cancel order request for order id {}", new Date(), orderId);
 
-        this.getTradeExecutor().cancel(username, orderId);
+        this.tradeExecutor.cancel(username, orderId);
     }
     
-    public abstract AccountController getAccountController();
+    public final AccountController getAccountController() {
+        return this.accntController;
+    }
 
-    public abstract  OrderBookProvider getOrderBookProvider();
+    public final OrderBookProvider getOrderBookProvider() {
+        return this.orderBookProvider;
+    }
 
-    public abstract TradeExecutor getTradeExecutor();
+    public final TradeExecutor getTradeExecutor() {
+        return this.tradeExecutor;
+    }
 
-    public abstract OrderProvider getOrderProvider();
+    public final OrderProvider getOrderProvider() {
+        return this.orderprovider;
+    }
 
+    protected abstract ExchangeEvent getEvent() throws ExchangeException;
+    
     public void getOpenOrders(List<LimitOrder> orders) {
         // could trace
-        this.getOrderBookProvider().getOpenOrders(orders);
+        this.orderBookProvider.getOpenOrders(orders);
     }
 
     public void getOrderBook(String username, OrderBook book, String tradeableIdentifier, String currency) {
         // could trace
-        this.getOrderBookProvider().getOrderBook(username, book, tradeableIdentifier, currency);
+        this.orderBookProvider.getOrderBook(username, book, tradeableIdentifier, currency);
     }
 
     public LimitOrder generateLimitOrder(String id, Order.OrderType type,
             double tradableAmount, String tradableIdentifier, String transactionCurrency, double price) {
-        return this.getOrderProvider().getLimitOrder(type, tradableAmount, tradableIdentifier, transactionCurrency, price);
+        return this.orderprovider.getLimitOrder(type, tradableAmount, tradableIdentifier, transactionCurrency, price);
     }
 
     public MarketOrder generateMarketOrder(String id, Order.OrderType type,
             double tradableAmount, String tradableIdentifier, String transactionCurrency) {
-        return this.getOrderProvider().getMarketOrder(type, tradableAmount, tradableIdentifier, transactionCurrency);
+        return this.orderprovider.getMarketOrder(type, tradableAmount, tradableIdentifier, transactionCurrency);
     }
 
     public AccountInfo getAccountInfo(String username) throws ExchangeException {
-        return this.getAccountController().getAccountInfo(username);
+        return this.accntController.getAccountInfo(username);
     }
 
     private Wallet getWallet(String username, String currency) throws ExchangeException {
@@ -200,12 +172,14 @@ public abstract class Exchange {
         return wallet.getBalance().getAmount().doubleValue();
     }
 
+    public abstract boolean isConnected();
+    
     public String placeOrder(String username, Order order) throws ExchangeException {
         // TODO: log impl with timestamps
         log.info("{} Place order request {}", new Date(), order);
 
         // For now let the order fail if there is not enough funds, etc.
-        return this.getTradeExecutor().execute(username, order);
+        return this.tradeExecutor.execute(username, order);
 
         // TBD: post-trade operations?
     }
@@ -216,8 +190,7 @@ public abstract class Exchange {
 
     public void start() {
         if (!this.started) {
-            this.streamingService.connect();
-            this.processor = new ExchangeEventProcessor(this.streamingService, this.listener);
+            this.processor = new ExchangeEventProcessor();
             this.processor.start();
             this.started = true;
         }
@@ -225,7 +198,6 @@ public abstract class Exchange {
 
     public void stop() {
         if (this.started) {
-            this.streamingService.disconnect();
             this.processor.running = false;
             // shouldn't have to interrupt
             this.processor = null;
--- src/com/zygon/trade/execution/exchange/mtgox/MtGoxExchange.java
+++ src/com/zygon/trade/execution/exchange/mtgox/MtGoxExchange.java
@@ -3,17 +3,19 @@ package com.zygon.trade.execution.exchange.mtgox;
 
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.ExchangeSpecification;
+import com.xeiam.xchange.mtgox.v2.dto.trade.polling.MtGoxOpenOrder;
+import com.xeiam.xchange.mtgox.v2.dto.trade.streaming.MtGoxTradeLag;
 import com.xeiam.xchange.mtgox.v2.service.polling.MtGoxPollingAccountService;
 import com.xeiam.xchange.mtgox.v2.service.polling.MtGoxPollingMarketDataService;
 import com.xeiam.xchange.mtgox.v2.service.polling.MtGoxPollingTradeService;
 import com.xeiam.xchange.mtgox.v2.service.streaming.MtGoxStreamingConfiguration;
 import com.xeiam.xchange.service.streaming.StreamingExchangeService;
-import com.zygon.trade.execution.AccountController;
-import com.zygon.trade.execution.OrderBookProvider;
-import com.zygon.trade.execution.OrderProvider;
-import com.zygon.trade.execution.TradeExecutor;
+import com.zygon.trade.execution.ExchangeException;
 import com.zygon.trade.execution.exchange.Exchange;
+import com.zygon.trade.execution.exchange.ExchangeEvent;
 import org.joda.money.CurrencyUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -21,15 +23,12 @@ import org.joda.money.CurrencyUnit;
  */
 public class MtGoxExchange extends Exchange {
 
+    private static final Logger log = LoggerFactory.getLogger(MtGoxExchange.class);
+    
     private static MtGoxStreamingConfiguration getStreamingConfig() {
         MtGoxStreamingConfiguration mtGoxStreamingConfiguration = new MtGoxStreamingConfiguration(10, 10000, 60000, true, "TODO");
         return mtGoxStreamingConfiguration;
     }
-    
-    private final MtGoxAcctController accntController;
-    private final OrderBookProvider orderBookProvider;
-    private final OrderProvider orderProvider;
-    private final TradeExecutor tradeExecutor;
     
     private static ExchangeSpecification getExchangeSpecification() {
         ExchangeSpecification spec = new ExchangeSpecification(com.xeiam.xchange.mtgox.v2.MtGoxExchange.class.getName());
@@ -44,38 +43,99 @@ public class MtGoxExchange extends Exchange {
         return ExchangeFactory.INSTANCE.createExchange(getExchangeSpecification()).getStreamingExchangeService(getStreamingConfig());
     }
     
+    private final StreamingExchangeService service;
+    private boolean isConnected = false;
+    
     public MtGoxExchange() {
-        super (getService());
+        super (new MtGoxAcctController(new MtGoxPollingAccountService(getExchangeSpecification())),
+               new MtGoxOrderBookProvider(new MtGoxPollingTradeService(getExchangeSpecification()), new MtGoxPollingMarketDataService(getExchangeSpecification())),
+               new MtGoxOrderProvider(CurrencyUnit.USD),
+               new MtGoxTradeExecutor(new MtGoxPollingTradeService(getExchangeSpecification())));
         
-        ExchangeSpecification spec = getExchangeSpecification();
+        this.service = getService();
+    }
+    
+    @Override
+    public ExchangeEvent getEvent() throws ExchangeException {
         
-        this.accntController = new MtGoxAcctController(new MtGoxPollingAccountService(spec));
+        ExchangeEvent.EventType eventType = null;
         
-        MtGoxPollingTradeService tradeService = new MtGoxPollingTradeService(spec);
-        MtGoxPollingMarketDataService marketDataService = new MtGoxPollingMarketDataService(spec);
-        
-        this.orderBookProvider = new MtGoxOrderBookProvider(tradeService, marketDataService);
-        this.orderProvider = new MtGoxOrderProvider(CurrencyUnit.USD);
-        this.tradeExecutor = new MtGoxTradeExecutor(tradeService);
-    }
+        try {
+            com.xeiam.xchange.service.streaming.ExchangeEvent event = this.service.getNextEvent();
 
-    @Override
-    public AccountController getAccountController() {
-        return this.accntController;
-    }
+            switch (event.getEventType()) {
 
-    @Override
-    public OrderBookProvider getOrderBookProvider() {
-        return this.orderBookProvider;
-    }
+                case CONNECT:
+                    if (!this.isConnected) {
+                        log.info("Connected to exchange");
+                        this.isConnected = true;
+                        // TBD: kill any connection task
+                    }
+                    break;
 
-    @Override
-    public OrderProvider getOrderProvider() {
-        return this.orderProvider;
-    }
+                case DISCONNECT:
+                    if (this.isConnected) {
+                        this.isConnected = false;
+                        log.info("Disconnected from exchange");
+                        // TBD: connection task that keeps trying to
+                        // to reconnect 
+                    }
 
+                    break;
+
+                case ERROR:
+                    log.info("Exchange error: " + event.getData());
+                    break;
+
+                case USER_ORDER:
+                    eventType = ExchangeEvent.EventType.TRADE_FILL;
+                    MtGoxOpenOrder order = (MtGoxOpenOrder) event.getPayload();
+                    log.info("User order: " + order);
+                    break;
+
+//                    case TICKER:
+//                      Ticker ticker = (Ticker) exchangeEvent.getPayload();
+//                      System.out.println(ticker.toString());
+//                      break;
+
+//                        case USER_ORDER
+//                            break;
+
+//                        case TRADE:
+//                            Trade trade = (Trade) event.getPayload();
+//                            // TODO: convert to propert exchange event
+//                            ExchangeEvent exchangeEvent = new ExchangeEvent();
+//                            this.listener.notify(exchangeEvent);
+//                            break;
+//
+                    case TRADE_LAG:
+                        MtGoxTradeLag lag = (MtGoxTradeLag) event.getPayload();
+                        //todo:
+                        break;
+
+//                    case DEPTH:
+//                      OrderBookUpdate orderBookUpdate = (OrderBookUpdate) exchangeEvent.getPayload();
+//                      System.out.println(orderBookUpdate.toString());
+//                      break;
+
+                default:
+                    // log?
+                    break;
+            }
+
+            if (eventType != null) {
+                return new ExchangeEvent(eventType);
+            }
+
+        } catch (InterruptedException ie) {
+            log.error(null, ie);
+        }
+
+        return null;
+    }
+    
     @Override
-    public TradeExecutor getTradeExecutor() {
-        return this.tradeExecutor;
+    public boolean isConnected() {
+        return this.isConnected;
     }
 }
