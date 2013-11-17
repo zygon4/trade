73972a733aea26e971ec0f54879e6673f344652e
zygon4 <dcharubini@gmail.com>
2013-11-17 00:40:53 -0500

    Add notes about expanding the account controller.

--- src/com/zygon/trade/execution/AccountController.java
+++ src/com/zygon/trade/execution/AccountController.java
@@ -11,5 +11,8 @@ import com.xeiam.xchange.dto.account.AccountInfo;
  * @author zygon
  */
 public interface AccountController {
+    // TBD: this probably needs to expand to allow for different account types.
+    // For example accounts hosted on target sites, or accounts hosted in 
+    // different banks, etc.
     public AccountInfo getAccountInfo(String username) throws ExchangeException;
 }
