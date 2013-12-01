58da3a4260c22aaaddd41c1f500dd352c8b1bbdb
zygon4 <dcharubini@gmail.com>
2013-11-30 19:22:04 -0500

    Let the kernel have some basic status information

--- src/com/zygon/trade/Kernel.java
+++ src/com/zygon/trade/Kernel.java
@@ -13,6 +13,11 @@ package com.zygon.trade;
  */
 public class Kernel extends Module {
 
+    private static final String PRODUCT_NAME = "Stella";
+    private static final String VERSION = "1.0";
+//    private static final String 
+    
+    
     public static final String ID = "kernel";
     
     private final String rootDirectory;
@@ -39,5 +44,13 @@ public class Kernel extends Module {
     @Override
     public void uninitialize() {
         // Nothing to do yet
+    }
+
+    @Override
+    protected void doWriteStatus(StringBuilder sb) {
+        sb.append(PRODUCT_NAME);
+        sb.append(" - ");
+        sb.append("v");
+        sb.append(VERSION);
     }
 }
