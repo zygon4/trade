af96c8e8dff83afee7594027d0ebc70cee571edc
zygon4 <dcharubini@gmail.com>
2014-10-26 00:36:37 -0400

    convert to guava collections

--- src/main/java/com/zygon/trade/ui/UIController.java
+++ src/main/java/com/zygon/trade/ui/UIController.java
@@ -6,12 +6,12 @@ package com.zygon.trade.ui;
 
 import asg.cliche.Shell;
 import asg.cliche.ShellFactory;
+import com.google.common.collect.Lists;
 import com.zygon.trade.ConfigurationCommandProcessor;
 import com.zygon.trade.ConfigurationManager;
 import com.zygon.trade.Module;
 import com.zygon.trade.OutputProviderImpl;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,7 +34,7 @@ public class UIController {
 
     private List<NavigationNode> buildNavigationTree(Module[] modules) {
         
-        List<NavigationNode> nodes = new ArrayList<>();
+        List<NavigationNode> nodes = Lists.newArrayList();
         
         if (modules != null) {
             for (Module module : modules) {
