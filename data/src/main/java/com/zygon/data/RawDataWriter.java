cfce18af64fb83ff7962a57011721f584af4de6a
zygon4 <dcharubini@gmail.com>
2015-12-05 01:25:03 -0500

    Moving data stuff to the data subproject (including cassandra)

--- data/src/main/java/com/zygon/data/RawDataWriter.java
+++ data/src/main/java/com/zygon/data/RawDataWriter.java
@@ -2,56 +2,54 @@
 package com.zygon.data;
 
 import com.google.gson.stream.JsonWriter;
-import com.zygon.trade.market.data.Ticker;
-import com.zygon.trade.market.data.TickerWriter;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.math.BigDecimal;
-import java.util.Date;
 
 /**
  *
  * @author zygon
+ * @param <T>
  */
 public class RawDataWriter<T> {
 
     private final String filePath;
-    
+
     public RawDataWriter(String filePath) {
         this.filePath = filePath;
     }
-    
+
     protected void doLog(T t, JsonWriter writer) throws IOException {
-        
+
     }
-    
+
     public synchronized void log (T t) throws IOException {
-        
+
         File outFile = new File(this.filePath);
-        
+
         if (!outFile.exists()) {
             outFile.createNewFile();
         }
-        
+
         FileOutputStream fos = null;
         JsonWriter writer = null;
-        
+
         try {
             fos = new FileOutputStream(this.filePath, true);
             StringWriter stringWriter = new StringWriter();
             writer = new JsonWriter(stringWriter);
-            
+
             doLog(t, writer);
-            
+
             String msg = stringWriter.toString();
             msg = msg.replace('\r', ' ');
             msg = msg.replace('\n', ' ');
-            
+
             fos.write(msg.getBytes());
             fos.write('\n');
-            
+
         } finally {
             if (writer != null) {
                 try { writer.close(); } catch (Throwable ignore) {}
@@ -61,21 +59,21 @@ public class RawDataWriter<T> {
             }
         }
     }
-    
-    public static void main (String[] args) throws IOException {
-        TickerWriter writer = new TickerWriter("/tmp/tmp-ticker.txt");
-        writer.log(
-            new Ticker(
-                "EUR", 
-                new Date(), 
-                "BOX", 
-                "USD", 
-                BigDecimal.valueOf(1.34), 
-                BigDecimal.valueOf(1.37), 
-                BigDecimal.valueOf(1.32), 
-                BigDecimal.valueOf(1.37), 
-                BigDecimal.valueOf(1.33), 
-                BigDecimal.valueOf(99.9)));
-    }
-    
+
+//    public static void main (String[] args) throws IOException {
+//        TickerWriter writer = new TickerWriter("/tmp/tmp-ticker.txt");
+//        writer.log(
+//            new Ticker(
+//                "EUR",
+//                new Date(),
+//                "BOX",
+//                "USD",
+//                BigDecimal.valueOf(1.34),
+//                BigDecimal.valueOf(1.37),
+//                BigDecimal.valueOf(1.32),
+//                BigDecimal.valueOf(1.37),
+//                BigDecimal.valueOf(1.33),
+//                BigDecimal.valueOf(99.9)));
+//    }
+
 }
